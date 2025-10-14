7247766df53f00da37fa681a31001f3a543d4333
gitseoyeon <tjdus553926@gmail.com>
2025-10-14 15:49:18 +0900

    init: 프로젝트 생성

---
+++ backend/plantory_be/src/main/java/org/example/plantory_be/PlantoryBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.plantory_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantoryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantoryBeApplication.class, args);
+    }
+
+}
---
+++ backend/plantory_be/src/test/java/org/example/plantory_be/PlantoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.plantory_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantoryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
