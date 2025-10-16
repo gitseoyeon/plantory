2769a5109ee7efe16d5786a7306ca8dac941f920
ᑭOOᖇEᑌᗰ.KIᗰ♡ <vnfma1229@naver.com>
2025-10-17 03:29:46 +0900

    유저 식물 Entity 작업 완

--- backend/plantory_be/src/main/java/org/example/plantory_be/dto/request/UserPlantRequest.java
+++ backend/plantory_be/src/main/java/org/example/plantory_be/dto/request/UserPlantRequest.java
@@ -1,4 +1,51 @@
 package org.example.plantory_be.dto.request;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.plantory_be.entity.PotSize;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserPlantRequest {
+
+    private Long species_id;
+
+    @NotBlank(message = "식물이름을 입력하세요")
+    @Size(max = 200, message = "이름은 200자를 초과 할 수 없습니다")
+    private String name;
+
+    @NotBlank(message = "닉네임을 입력하세요")
+    @Size(max = 200, message = "닉네임은 200자를 초과 할 수 없습니다")
+    private String petName;
+
+    private LocalDate acquiredDate;
+
+    private String imageUrl;
+
+    private boolean Indoor ;
+
+    private String location;
+
+    private  String store;
+
+    private BigDecimal price;
+
+    private PotSize potSize;
+
+    private String qrUrl;
+
+    private String qrImageUrl;
+
 }
--- backend/plantory_be/src/main/java/org/example/plantory_be/entity/PotSize.java
+++ backend/plantory_be/src/main/java/org/example/plantory_be/entity/PotSize.java
@@ -1,4 +1,18 @@
 package org.example.plantory_be.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum PotSize {
+    XS("XS (너비 10cm 이하)"),
+    S("S (너비 10 ~ 20cm)"),
+    M("M (너비 20 ~ 50cm)"),
+    L("L (너비 50 ~ 100cm)"),
+    XL("XL (너비 100cm 이상)");
+
+    private final String label;
+
+    PotSize(String label) {
+        this.label = label;
+    }
 }
