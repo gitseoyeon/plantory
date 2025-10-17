9a146d36e811384d93179a5fb8a7fc0687f8f61b
gitseoyeon <tjdus553926@gmail.com>
2025-10-18 02:20:37 +0900

    rename: 로그인한 유저 정보 반환 DTO 명 변경

---
+++ backend/plantory_be/src/main/java/org/example/plantory_be/dto/response/UserPrivateProfileResponse.java
@@ -0,0 +1,35 @@
+package org.example.plantory_be.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.plantory_be.entity.User;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPrivateProfileResponse {
+    private String email;
+    private String username;
+    private String nickname;
+    private String profileImageUrl;
+    private String bio;
+    private String experience;
+    private String interest;
+    private String style;
+
+    public static UserPrivateProfileResponse fromEntity(User user) {
+        return UserPrivateProfileResponse.builder()
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .nickname(user.getNickName())
+                .profileImageUrl(user.getProfileImageUrl())
+                .bio(user.getBio())
+                .experience(user.getExperience())
+                .interest(user.getInterest())
+                .style(user.getInterest())
+                .build();
+    }
+}
