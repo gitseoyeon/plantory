cb807ed805438a30973da4bc901dae49a3f73b53
juyoung <juyoungjeong.797@gmail.com>
2025-10-17 17:02:26 +0900

    Chore: Remove unused imports

--- backend/plantory_be/src/main/java/org/example/plantory_be/repository/CommentRepository.java
+++ backend/plantory_be/src/main/java/org/example/plantory_be/repository/CommentRepository.java
@@ -1,11 +1,4 @@
 package org.example.plantory_be.repository;
 
-import org.example.plantory_be.entity.Comment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 public interface CommentRepository {
 }
