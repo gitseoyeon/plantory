934d902dbf1c5ac0d7de61b5bd397e88e6e6ba8e
gitseoyeon <tjdus553926@gmail.com>
2025-10-23 03:16:58 +0900

    fix: 사용자 이름 변경 가능, 닉네임 불가능 조건 수정

--- frontend/src/pages/myPage.jsx
+++ frontend/src/pages/myPage.jsx
@@ -255,8 +255,8 @@ const EditProfileForm = ({ user, onUpdateSuccess, onCancel }) => {
           type="text"
           name="username"
           value={formData.username}
-          disabled
-          className="w-full px-4 py-2 border border-gray-300 rounded-lg bg-gray-100 cursor-not-allowed"
+          onChange={handleChange}
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-300 outline-none"
         />
       </div>
 
@@ -281,8 +281,8 @@ const EditProfileForm = ({ user, onUpdateSuccess, onCancel }) => {
           type="text"
           name="nickname"
           value={formData.nickname}
-          onChange={handleChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-300 outline-none"
+          disabled
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg  bg-gray-100 cursor-not-allowed"
         />
       </div>
 
